7120ceb14e1edb3292373f29ed4fa52d0dd4a56d
Vegar Ringdal <vegar.ringdal@gmail.com>
2020-03-21 13:33:15 +0100

    update 1.0.4

--- dynamic/src-routes-cable.053699775.js
+++ dynamic/src-routes-cable.053699775.js
@@ -1,1 +1,1 @@
-__fuse.bundle({10:function(e,t,n){var s=e(13);t.__esModule=!0;var l=e(12),r=e(14);let a=class extends HTMLElement{render(){return l.html`<section class="text-gray-200 p-2 text-center">🥺 this is not made yet 🙉</section>`}};a=s.d([r.customElement("cable-route")],a),t.default=a}});
+__fuse.bundle({12:function(e,t,n){var s=e(6);t.__esModule=!0;var l=e(7),r=e(8);let a=class extends HTMLElement{render(){return l.html`<section class="text-gray-200 p-2 text-center">🥺 this is not made yet 🙉</section>`}};a=s.d([r.customElement("cable-route")],a),t.default=a}});
